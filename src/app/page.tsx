6b0a3b12ea7692845b63b289f15d71361049a9c0
RahimGuerfi <89612748+RahimGuerfi@users.noreply.github.com>
2023-12-31 18:54:36 +0100

    feat: Init Next.js porject with prettier, tailwind, eslint and shadcn ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-full">
+      <section className=" p-5">
+        <p>TESTING FONT...</p>
+      </section>
+    </main>
+  );
+}
